e5ce87a92589e9f8cbe9cf924a8f1a4d1db1495b
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-05-08 23:51:32 +0700

    add code to controll music

--- app/src/main/java/com/example/exerciseservice/MainActivity.java
+++ app/src/main/java/com/example/exerciseservice/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.exerciseservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.IBinder;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -29,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
     private ListView list;
     private ArrayList<Song> songList;
 
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +57,36 @@ public class MainActivity extends AppCompatActivity {
         list.setAdapter(songAdt);
 
     }
+
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            //get service
+            musicSrv = binder.getService();
+            //pass list
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(playIntent==null){
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
