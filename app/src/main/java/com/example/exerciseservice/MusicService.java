cd5a0e029a0bb948d77d5249dbbdc07a462c2667
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-05-08 23:47:49 +0700

    add code in MusicService.java

---
+++ app/src/main/java/com/example/exerciseservice/MusicService.java
@@ -0,0 +1,82 @@
+package com.example.exerciseservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MusicService extends Service implements
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
+    private MediaPlayer player;
+    //song list
+    private ArrayList<Song> songs;
+    //current position
+    private int songPosn;
+    private final IBinder musicBind = new MusicBinder();
+    private String songTitle = "";
+    private static final int NOTIFY_ID = 1;
+    private boolean shuffle = false;
+    private Random rand;
+
+    public void onCreate() {
+        //create the service
+        //create the service
+        super.onCreate();
+//initialize position
+        songPosn = 0;
+//create player
+        player = new MediaPlayer();
+        rand = new Random();
+    }
+    public void initMusicPlayer() {
+        //set player properties
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+
+    public void setList(ArrayList<Song> theSongs){
+        songs=theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
+}
